a16691e645d44278cfaed5e3cd89ed380a684ffb
YevgeniiShvets221 <54836319+YevgeniiShvets221@users.noreply.github.com>
2022-03-04 21:52:13 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    byte age = 21;
+        System.out.println("My age is " + age+ " years old.");
+        short birth_year = 2000;
+        System.out.println("The year of my birth is "+birth_year+ " and it's 21st century:)");
+        int city_population = 1010000;
+        System.out.println("Population of my native city Odessa is "+city_population+" inhabitants!!!");
+        long views_number = 7764227078l;
+        System.out.println("My favourite song has "+views_number+" views on YouTube:)");
+        float height = 1.87f;
+        System.out.println("My height is "+height+" metres.");
+        double apartment_cost = 5000.0 /30;
+        System.out.println("Middle price of apartment, where I lived is about "+apartment_cost+" grn. per day.");
+        char salary = '$';
+        System.out.println("Middle salary in my region is about 500"+salary);
+        boolean studying = true;
+        System.out.println("The fact, that I'm studying from 2006 is "+studying);
+
+        
+    }
+}
